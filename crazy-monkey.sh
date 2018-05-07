3da278a8d29d78dfc8dd7689f7f0a20ecced1665
tgropper <tomasgropper@gmail.com>
2018-05-07 12:35:47 -0300

    add parallel execution

--- crazy-monkey.sh
+++ crazy-monkey.sh
@@ -4,10 +4,12 @@ USAGE='Usage: ./crazy-monkey.sh [OPTIONS]
 \n\nOptions:
 \n\t --dead-time \t Sets the time the dead container should remain stopped (default 1)
 \n\t --sleep-time \t Sets the time between each kill (default 5)
+\n\t --parallel \t Defines the number of parallel executions (default 3)
 \n\nRuns a crazy-monkey that randomly kills a running docker container.'
 
 DEADTIME=1
 SLEEPTIME=5
+PARALLEL=3
 
 for i in "$@"
 do
@@ -22,13 +24,15 @@ case $i in
 --sleep-time=*)
   SLEEPTIME="${i#*=}"
   ;;
+--parallel=*)
+  PARALLEL="${i#*=}"
+  ;;
 esac
 done
 
 echo "Running crazy-monkey with parameters: dead-time=$DEADTIME; sleep-time=$SLEEPTIME."
 
-while true
-do
+kill () {
   DEADID=$(docker ps -q | xargs shuf -n1 -e)
   DEADNAME=$(docker ps --format '{{.ID}} {{.Names}}' | grep $DEADID | awk '{print $2}')
 
@@ -36,6 +40,12 @@ do
   docker stop $DEADID > /dev/null
   sleep $DEADTIME
   docker start $DEADID > /dev/null
-  echo "Container $DEADNAME is back alive. Now it's safe to exit crazy-monkey."
+  echo "Container $DEADNAME is back alive."
+}
+
+while true
+do
+  for ((i=1; i<=$PARALLEL; i++)); do; kill &; done; wait
+  echo "Killed containers are back alive. Now it's safe to exit crazy-monkey."
   sleep $SLEEPTIME
 done
