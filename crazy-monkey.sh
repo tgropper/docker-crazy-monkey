b72a864a048b1346456bb4e7b183f79a5b63fd32
tgropper <tomasgropper@gmail.com>
2018-05-17 14:44:41 -0300

    Add containers regex

--- crazy-monkey.sh
+++ crazy-monkey.sh
@@ -5,11 +5,13 @@ USAGE='Usage: ./crazy-monkey.sh [OPTIONS]
 \n\t --dead-time \t Sets the time the dead container should remain stopped (default 1)
 \n\t --sleep-time \t Sets the time between each kill (default 5)
 \n\t --parallel \t Defines the number of parallel kill executions (default 3)
+\n\t --containers-regex \t Regex to define the list of containers to use (default all)
 \n\nRuns a crazy-monkey that randomly kills running docker containers.'
 
 DEADTIME=1
 SLEEPTIME=5
 PARALLEL=3
+REGEX="."
 
 for i in "$@"
 do
@@ -27,14 +29,20 @@ case $i in
 --parallel=*)
   PARALLEL="${i#*=}"
   ;;
+--containers-regex=*)
+  REGEX="${i#*=}"
+  ;;
 esac
 done
 
-echo "Running crazy-monkey with parameters: dead-time=$DEADTIME; sleep-time=$SLEEPTIME; parallel=$PARALLEL."
+echo "Running crazy-monkey with parameters: dead-time=$DEADTIME; sleep-time=$SLEEPTIME; parallel=$PARALLEL; regex=$REGEX."
+
+CONTAINERS=$(docker ps -a --format '{{.Names}} {{.ID }}' | grep "$REGEX")
 
 kill () {
-  DEADID=$(docker ps -q | xargs shuf -n1 -e)
-  DEADNAME=$(docker ps --format '{{.ID}} {{.Names}}' | grep $DEADID | awk '{print $2}')
+  # DEADID=$(docker ps -q | xargs shuf -n1 -e)
+  DEADID=$($CONTAINERS | awk '{print $ 2}' | xargs shuf -n1 -e)
+  DEADNAME=$($CONTAINERS | grep $DEADID | awk '{print $1}')
 
   trap 'start $DEADID $DEADNAME; exit 1' INT
 
